9be6afda7dfb691c9ec66468b367b15112c8fb12
Bruno Sacco <brusacco@gmail.com>
2023-11-02 16:27:05 -0300

    dd

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -4,7 +4,7 @@ class EntryController < ApplicationController
   def show; end
 
   def popular
-    @entries = Entry.joins(:site).where(total_count: 1..).a_day_ago.where.not(image_url: nil).order(total_count: :desc).limit(50)
+    @entries = Entry.joins(:site).where(total_count: 1..).a_day_ago.where.not(image_url: nil).order(total_count: :desc)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
     # Cosas nuevas
