d4e469b1a0e775809a347dd274c4e3cae0f41cc4
Bruno Sacco <brusacco@gmail.com>
2023-11-03 13:20:05 -0300

    aa

--- lib/tasks/site_crawler.rb
+++ lib/tasks/site_crawler.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 desc 'Moopio Morfeo web crawler'
-task site_crawler: :environment do
+task site: :environment do
   site_id = ENV.fetch('site_id')
 
   if site_id.blank?
