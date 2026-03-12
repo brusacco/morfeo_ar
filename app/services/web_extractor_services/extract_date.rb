d3f5087bb5e9bc1638290ca263333701a5c9d65a
Bruno Sacco <brusacco@gmail.com>
2026-03-12 00:24:20 -0300

    Add additional date extraction logic for improved parsing

--- app/services/web_extractor_services/extract_date.rb
+++ app/services/web_extractor_services/extract_date.rb
@@ -61,6 +61,9 @@ module WebExtractorServices
       elsif @doc.at('.noticia_fecha') && @date.nil?
         @date = @doc.at('.noticia_fecha').text
         @parsed = false
+      elsif @doc.at('.fa-calendar') && @date.nil?
+        @date = @doc.at('.fa-calendar').text
+        @parsed = false
       elsif @doc.at('.title-post li:nth-child(1)') && @date.nil?
         @date = @doc.at('.title-post li:nth-child(1)').text
         @parsed = false
